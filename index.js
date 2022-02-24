4599e9775ef870de26a98f5f6d3ee05aec2707e2
Munkhamgalan <Munkhamgalan-Y1@CC0096.local>
2022-02-24 18:16:37 +0800

    work

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function displayTime() {
+    var dateTime = new Date();
+    document.getElementById('hours').textContent = dateTime.getHours()+":"+dateTime.getMinutes()+":"+dateTime.getSeconds();
+    setInterval(displayTime, 1000);
+}
+displayTime()
 
+localStorage.setItem('theme', 'theme-dark');
+    document.documentElement.className = 'theme-dark';
+
+function setTheme(themeName){
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark'){
+        setTheme('theme-light');
+    }else{
+        setTheme('theme-dark')
+    }
+}
+(function () {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-dark');
+    } else {
+        setTheme('theme-light');
+    }
+})();
