050974b528d1c0b7a1a06580d8c76bb1a391f60b
Batkhuyag Bavuudorj <Batkhuyag-Y1@iMac2021-07s-iMac.local>
2022-02-24 17:45:13 +0800

    done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const root = document.documentElement
+const time = document.querySelector(".time")
+const button = document.querySelector(".button")
 
+function updateTime(){
+    let date = new Date
+    time.textContent = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
+    setTimeout(updateTime, 500)
+}
+updateTime()
+
+document.documentElement.className = "theme-classic"
+
+function switchTheme(){
+    let theme
+    switch (document.documentElement.className) {
+        case "theme-classic":
+            theme = "theme-light"
+            break;
+        case "theme-light":
+            theme = "theme-dark"
+            break;
+        default:
+            theme = "theme-classic"
+            break;
+    }
+    document.documentElement.className = theme
+}
