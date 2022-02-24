671024dd7176ca4e2e9db7d01a1f061bc7405819
Bilguun <bilguun-y1@iMac2021-08s-iMac.local>
2022-02-24 17:50:58 +0800

    did some changes

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function updateTime() {
+      let date = new Date();
+      document.getElementById("tsag").innerHTML = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+      setTimeout(updateTime, 500);
+}
+updateTime();
 
+function setTheme(themeName) {
+      localStorage.setItem('theme', themeName);
+      document.documentElement.className = themeName;
+}
+function switcher() {
+      if (localStorage.getItem('theme') === 'theme-dark') {
+          setTheme('theme-light');
+      } else {
+          setTheme('theme-dark');
+      }
+}
+(function () {
+      if (localStorage.getItem('theme') === 'theme-dark') {
+          setTheme('theme-dark');
+      } else {
+          setTheme('theme-light');
+      }
+})();
