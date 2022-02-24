1df05e54c5a020b92e216dfd99bbae9604ecd708
buyantogtoh-y1 <buyantogtoh-y1@iMac-17s-iMac.local>
2022-02-24 17:32:29 +0800

    issue3

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const a = new Date();
-let time =  a.getHours() + ":" + a.getMinutes();
-console.log(time);
-const clock = document.querySelector(".clock");
-clock.textContent = time;
+/* onst a = new Date();
+let time =  a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+const clock = document.querySelector("#clock");
+clock.textContent = time; 
+setTimeout(time, 1000); */
+function tsag() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    const time = h + ":" + m + ":" + s;
+    const clock = document.querySelector("#clock");
+    clock.textContent = time;
+    setTimeout(tsag, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  
+    return i;
+}
